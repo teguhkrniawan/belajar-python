712a848f8c9352ac00619e43f7b1616809940e9d
Teguh Kurniawan <teguhkrniawan@gmail.com>
2023-07-02 10:59:53 +0700

    OK

--- Main.py
+++ Main.py
@@ -1,8 +1,21 @@
-# input
+# date time
 
-panjang = int(input("Masukkan panjang : " ))
-lebar   = int(input("Masukkan lebar : " ))
+import datetime as dt
 
-result = panjang * lebar
-print("luas persegi panjang ", result)
+print("Masukkan tgl lahir anda :")
+tanggal     = int(input())
+print("Masukkan bulan lahir anda :")
+bulan       = int(input())
+print("Masukkan tahun lahir anda :")
+tahun       = int(input())
+
+tgl_lahir = dt.date(tahun, bulan, tanggal)
+print('tgl lahir anda, ', tgl_lahir)
+
+hari_ini    = dt.date.today()
+umur_hari   = hari_ini - tgl_lahir
+umur_tahun  = umur_hari.days // 365 # operator tsb untuk dapetin nilai integernya
+
+
+print(f"Umur kamu saat ini {umur_tahun} tahun")
 
