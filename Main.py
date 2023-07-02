b11f2e0999895a429420b91bb3ec2d5845de064f
Teguh Kurniawan <teguhkrniawan@gmail.com>
2023-07-02 10:21:10 +0700

    tipe data ok

--- Main.py
+++ Main.py
@@ -1,9 +1,4 @@
-panjang = 20
-lebar   = 30
-
-luas_persegi_panjang = panjang * lebar
-print("Luas Persegi Panjang adalah : ", luas_persegi_panjang , "m2")
-
-panjang = 30
-luas_persegi_panjang = panjang * lebar
-print("luas setelah diubah : ", luas_persegi_panjang)
+# TIPE DATA
+data_complex = complex(5, 6)
+print(data_complex)
+print(type(data_complex))
