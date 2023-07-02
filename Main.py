4906fa8e156681f237f9b3971625051cf60cf18a
Teguh Kurniawan <teguhkrniawan@gmail.com>
2023-07-02 10:17:05 +0700

    variabel ok

---
+++ Main.py
@@ -0,0 +1,9 @@
+panjang = 20
+lebar   = 30
+
+luas_persegi_panjang = panjang * lebar
+print("Luas Persegi Panjang adalah : ", luas_persegi_panjang , "m2")
+
+panjang = 30
+luas_persegi_panjang = panjang * lebar
+print("luas setelah diubah : ", luas_persegi_panjang)
