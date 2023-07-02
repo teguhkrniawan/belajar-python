48046a70d04267a8821d61ff19fcc4557cf9eca2
Teguh Kurniawan <teguhkrniawan@gmail.com>
2023-07-02 11:28:23 +0700

    OK

--- Main.py
+++ Main.py
@@ -1,21 +1,20 @@
-# date time
+# IF ELSE STATEMENT
 
-import datetime as dt
+print("PILIH OPERATOR BERIKUT, DENGAN KETIKKAN ANGKA")
+print("1. TAMBAH")
+print("2. KURANG")
 
-print("Masukkan tgl lahir anda :")
-tanggal     = int(input())
-print("Masukkan bulan lahir anda :")
-bulan       = int(input())
-print("Masukkan tahun lahir anda :")
-tahun       = int(input())
+pilihan_user = int(input("PILIHAN SAYA :\t"))
 
-tgl_lahir = dt.date(tahun, bulan, tanggal)
-print('tgl lahir anda, ', tgl_lahir)
-
-hari_ini    = dt.date.today()
-umur_hari   = hari_ini - tgl_lahir
-umur_tahun  = umur_hari.days // 365 # operator tsb untuk dapetin nilai integernya
-
-
-print(f"Umur kamu saat ini {umur_tahun} tahun")
-
+if pilihan_user > 2:
+        print("TIDAK ADA PILIHAN TERSEBUT")
+if pilihan_user == 1:
+        angka1 = int(input("ANGKA 1 : "))
+        angka2 = int(input("ANGKA 2 : "))
+        result = angka1 + angka2
+        print("HASIL TAMBAH : ", result)
+if pilihan_user == 2:
+        angka1 = int(input("ANGKA 1 : "))
+        angka2 = int(input("ANGKA 2 : "))
+        result = angka1 - angka2
+        print("HASIL KURANG : ", result)
